4219c52cd1207f75f5a37f3f76d4cf10582c5ffe
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-11 23:00:37 +0500

    admin panel setting

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Category, New
 
-admin.site.register(New)
-admin.site.register(Category)
 
-# Register your models here.
+@admin.register(New)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "category", "publish_time", "status"]
+    list_filter = ["publish_time", "status", "category"]
+    # prepopulated_fields = {'slug' : ('title', )}
+    date_hierarchy = 'publish_time'
+    search_fields = ['title' , 'body']
+    ordering = ['category']
+
+class CategoryAdmin(admin.ModelAdmin) :
+    list_display = ['id' , 'name']
+     
+admin.site.register(Category , CategoryAdmin)    
+
--- news_app/models.py
+++ news_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
-class Category(models.Model) :
+
+class Category(models.Model):
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
 
 class New(models.Model):
 
@@ -11,18 +17,24 @@ class New(models.Model):
         Published = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
     body = models.TextField()
     image = models.ImageField(upload_to="news/images")
-    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     publish_time = models.DateTimeField(default=timezone.now)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2 , choices=Status.choices , default=Status.Draft)
-    
-    class Meta :
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.Draft
+    )
+
+    class Meta:
         ordering = ["-publish_time"]
-        
+
     def __str__(self):
         return self.title
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
