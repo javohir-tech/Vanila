dae32f4902e58ff53c4428a37c0cfc6bafa9f89b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-13 00:34:06 +0500

    update

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import New
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def news_list(request) :
+    news_list = New.objects.all()
+    context = {
+        'new_list' : news_list
+    }
+
+    return render(request , 'news/news_list.html'  , context=context)
+
+
+def news_detail(request , id) :
+    new_list =get_object_or_404(New , id=id)
+
+    context = {
+        'news' : new_list
+    }
+    
+    return render(request , "news/news_detail.html" , context=context)
--- news_project/settings.py
+++ news_project/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'news_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
