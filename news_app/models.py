f7f6927667bc13f1466b79a85b6c386c23394e37
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-12 22:55:30 +0500

    update

--- news_app/models.py
+++ news_app/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
-
-class PublishedManager(models.Manager) :
-    
-    def get_queryset(self) :
-        return super().get_queryset().filter(status = New.Status.Published)
+from .managers import PublishedManager
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
