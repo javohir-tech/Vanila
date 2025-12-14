f68dabe4262f295b6dde9f7f545003bc92a34e18
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-14 13:10:28 +0500

    update

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import  news_list , news_detail , indexViews
+from .views import  (
+    news_list ,
+    news_detail ,
+    indexViews,
+    ContactView,
+    ErrorView
+)
 
 urlpatterns = [
-    path('' , indexViews , name='index'),
+    path('' , indexViews , name='home'),
     path("all/" , news_list, name = 'all_news_list'),
-    path('news/<int:id>/' , news_detail , name='news_detail')
+    path('news/<int:id>/' , news_detail , name='news_detail'),
+    path('contact-us/' , ContactView , name='contact-us'),
+    path('404/' , ErrorView , name='404')
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,34 +1,46 @@
 from django.shortcuts import render
-from .models import New , Category
+from .models import New, Category
 from django.shortcuts import get_object_or_404
 
-def news_list(request) :
+
+def news_list(request):
     news_list = New.objects.all()
     context = {
-        'new_list' : news_list
+        'new_list': news_list
     }
 
-    return render(request , 'news/news_list.html'  , context=context)
+    return render(request, 'news/news_list.html', context=context)
 
 
-def news_detail(request , id) :
-    new_list =get_object_or_404(New , id=id)
+def news_detail(request, id):
+    new_list = get_object_or_404(New, id=id)
 
     context = {
-        'news' : new_list
+        'news': new_list
     }
-    
-    return render(request , "news/news_detail.html" , context=context)
+
+    return render(request, "news/news_detail.html", context=context)
 
 
-def indexViews(request) :
-    news_list =  New.objects.all()
+def indexViews(request):
+    news_list = New.objects.all()
     categories = Category.objects.all()
 
     context = {
-        'news_list' : news_list,
-        'categories' : categories
+        'news_list': news_list,
+        'categories': categories
     }
 
-    return render(request , 'news/index.html' , context = context)
+    return render(request, 'news/index.html', context=context)
 
+
+def ContactView(request):
+    context = {
+
+    }
+
+    return render(request, 'news/contact.html', context)
+
+
+def ErrorView(request):
+    return render(request, 'news/404.html', {})
