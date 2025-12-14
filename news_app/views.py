8bf480219f1690e6493398a122e65408ed5e3b45
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-14 18:04:00 +0500

    update

--- news_app/views.py
+++ news_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import New, Category
+from .models import New, Category, Contact
 from django.shortcuts import get_object_or_404
-
+from .forms import ContactForm
+from django.http import HttpResponse
 
 def news_list(request):
     news_list = New.objects.all()
@@ -35,12 +36,18 @@ def indexViews(request):
 
 
 def ContactView(request):
-    context = {
-
-    }
-
-    return render(request, 'news/contact.html', context)
-
+   form = ContactForm(request.POST or None)
+   if request.method == 'POST' and form.is_valid():
+       form.save()
+       return HttpResponse("<h2>Habar jonatildi</h2>")
+   context = {
+       "form" : form
+   }
+   
+   return render(request , "news/contact.html" , context)
 
 def ErrorView(request):
     return render(request, 'news/404.html', {})
+
+
+
