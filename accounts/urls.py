5c04b314584c391ea663efa6679d273842335d0f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-21 00:42:10 +0500

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import login_view
+from django.contrib.auth.views import LoginView , LogoutView
 
+    # path('login/' , login_view , name='login')
 urlpatterns = [
-    path('login/' , login_view , name='login')
+    path('login/' , LoginView.as_view() , name='login'),
+    path('logout/', LogoutView.as_view(), name="logout")
 ]
+
+
