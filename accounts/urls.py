9a33c6817dd9ec5a3383f253e60b34fc906d98c4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-21 15:28:56 +0500

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,41 @@
 from django.urls import path
-from .views import login_view , dashbord_view
-from django.contrib.auth.views import LoginView , LogoutView , PasswordChangeView , PasswordChangeDoneView
-from django.urls import reverse_lazy
+from .views import login_view, dashbord_view
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
-    # path('login/' , login_view , name='login')
+# path('login/' , login_view , name='login')
 urlpatterns = [
-    path('login/' , LoginView.as_view() , name='login'),
-    path('logout/', LogoutView.as_view(), name="logout"),
-    path('profile/' , dashbord_view , name='profile'),
-    path('password-change/' , PasswordChangeView.as_view() , name='password_change'),
-    path('password-change-done/' , PasswordChangeDoneView.as_view() , name='password_change_done')
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", dashbord_view, name="profile"),
+    path("password-change/", PasswordChangeView.as_view(), name="password_change"),
+    path(
+        "password-change-done/",
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password-reset_done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-complate/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
-
-
--- news_project/settings.py
+++ news_project/settings.py
@@ -123,3 +123,5 @@ MEDIA_ROOT = BASE_DIR / "media/"
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
